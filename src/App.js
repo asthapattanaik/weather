7af006ceac1712dc8bd3370764f83041486584a5
Astha Pattanaik <astha.pattanaik@truminds.com>
2023-08-30 13:25:16 +0530

    weather predictor added

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
+          See Source Code
+        </a>{" "}
+        | Developed by{" "}
+        <a target="_blank">
+          Astha Pattanaik
+        </a>{" "}
+        
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  key: "5b6d1550b02913e8fa1eeca6fb826d4a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
