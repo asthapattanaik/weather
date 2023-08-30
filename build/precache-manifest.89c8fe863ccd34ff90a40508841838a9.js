99e8525ae0b0fb95957db1a0231b9a1beb799cea
Astha Pattanaik <astha.pattanaik@truminds.com>
2023-08-30 13:50:23 +0530

    weather predictor added

---
+++ build/precache-manifest.89c8fe863ccd34ff90a40508841838a9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d017db1712aa4a4979b42da31e74d77f",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "231a434e77ecb6472c76",
+    "url": "/weather/static/css/main.3d18e216.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/weather/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "231a434e77ecb6472c76",
+    "url": "/weather/static/js/main.97ef9fee.chunk.js"
+  },
+  {
+    "revision": "93f41cbaddd6189c9424",
+    "url": "/weather/static/js/runtime-main.b405a412.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/weather/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "62c7a47b253f3108f95bdbe9883fc35f",
+    "url": "/weather/static/media/background1.62c7a47b.jpg"
+  },
+  {
+    "revision": "4f1ab76e1f95ce052188c5a75810fe66",
+    "url": "/weather/static/media/city1.4f1ab76e.jpg"
+  }
+]);
